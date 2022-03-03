9d73fc36bbb4cbb9273c2ac4e67135fce667d53e
Axelxs <88009984+Axelxs@users.noreply.github.com>
2022-03-03 14:56:38 -0600

    Update menu-scripts.py

--- menu-scripts.py
+++ menu-scripts.py
@@ -273,7 +273,6 @@ elif choice == 10:
     print("El bot esta abierto")
     TOKEN = "5203289182:AAEa7KpNV4Fsn2NgUkJWXrpLD1YOlEYk_kc" 
     bot = telebot.TeleBot(TOKEN)  
-    @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
         bot.reply_to(message, "")
     @bot.message_handler(func=lambda message: True)
