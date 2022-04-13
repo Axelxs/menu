f8db8640b4d1bd2f74e86614d2ca503766f68c4b
Axelxs <88009984+Axelxs@users.noreply.github.com>
2022-04-12 22:07:48 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
 import requests
+from time import sleep
+from os import name, system
 import json
 import urllib.request as request
 import ssl
@@ -24,9 +26,9 @@ from datetime import datetime
 import binascii
 import ipaddress
 import platform
-
-os.system("clear")
-
+os.system('pip install requests')
+os.system('pip install telebot')
+os.system('clear')
 RR = "\033[29;m"
 def a(s):
         for c in s + '\n':
@@ -46,33 +48,130 @@ a(RR+" instagram: a30xp30")
 a(RR+" YouTube: a..x..e..l..x..s")
 print("")
 a(RR+"╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱")
-choice = int(input("1.-BOTNET  TIKTOK\n2.-ESCANEO WEP\n3.-spam de correos gmail\n4.-info de IP publica\n5.-Servidor privado http\n6.-Mi IP privada \n7.-info basica de cualquier tipo de ip \n8.-Info de un numero\n9.-Barrido de IP\n10.-BotChat\n11.-TelegramBot\n12.-DDOS\n13.-codigo fuente de una wep\n14.-SSL wep\nSeleccione una opsion: ")) 
+choice = int(input("1.-TIKTOK REPORT BOT\n2.-ESCANEO WEP\n3.-spam de correos gmail\n4.-info de IP publica\n5.-Servidor privado http\n6.-Mi IP privada \n7.-info basica de cualquier tipo de ip \n8.-Info de un numero\n9.-Barrido de IP\n10.-BotChat\n11.-TelegramBot\n12.-DDOS\n13.-codigo fuente de una wep\n14.-SSL wep\n15.-Usar bash\nSeleccione una opsion: ")) 
 b = 1         
 if choice == 1:
-  op = int(input("1.-atake consentrado\n2.-atake bruto\nSeleccione una opsion:"))  
-  if op == 1:
-   os.system("clear")
-   a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
-   a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")
-   a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
-   print("\033[;32m")
-   username = ""
-   username = input("usuario:")
-   while True:
-         print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
-         sleep(1)
-         b = b + 1
-  elif op == 2:
-   os.system("clear")
-   a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
-   a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")
-   a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
-   print("\033[;32m")
-   username = ""
-   username = input("usuario:")
-   while True:
-         print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
-         b = b + 1  
+  cv = int(input("1.-simulador de report\n2.-Real\nSelecciona una opsion: "))
+  if cv == 1:
+      op = int(input("1.-atake consentrado\n2.-atake bruto\nSeleccione una opsion:"))  
+      if op == 1:
+       os.system("clear")
+       a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
+       a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")
+       a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
+       print("\033[;32m")
+       username = ""
+       username = input("usuario:")
+       while True:
+             print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
+             sleep(1)
+             b = b + 1
+      elif op == 2:
+       os.system("clear")
+       a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
+       a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")
+       a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
+       print("\033[;32m")
+       username = ""
+       username = input("usuario:")
+       while True:
+             print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
+             b = b + 1  
+
+  elif cv == 2:
+
+        col = "\033[32;m"
+        
+        try:
+          import requests
+        except ImportError:
+            os.system('python -m pip install requests')
+        try:
+          import urllib3
+        except ImportError:
+            os.system('python -m pip install urllib3')
+        try:
+          from bs4 import BeautifulSoup
+        except ImportError:
+            os.system('python -m pip install bs4')
+        
+        def clear():
+            if name == 'nt':
+                _ = system('cls')
+            else:
+                _ = system('clear')
+        
+        clear()
+        a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
+        a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")
+        a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
+        
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        
+        srttm = time.time()
+        
+        usr = input("[?] Username: ")
+        
+        if usr.startswith("@"):
+            usr = usr
+        else:
+            usr = f"@{usr}"
+        
+        r0 = requests.get(f"https://www.tiktok.com/{usr}", headers={"User-Agent":"Mozilla/5.0 (Linux; Android 11; M2102J20SG Build/RQ3A.210705.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/93.0.4577.62 Mobile Safari/537.36 trill_2022002050 JsSdk/1.0 NetType/WIFI Channel/googleplay AppName/musical_ly app_version/20.2.5 ByteLocale/en ByteFullLocale/en Region/US"})
+        soup = BeautifulSoup(r0.text, "html.parser")
+        content = soup.find_all(attrs={"id":"__NEXT_DATA__"})
+        content = json.loads(content[0].contents[0])
+        profile_data = {
+            "uid":content['props']['pageProps']['userInfo']['user']['id']
+            }
+        
+        uid = f"{profile_data.get('uid')}"
+        
+        print("\n", end="")
+        
+        def report():
+            h1 = {
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "en-US",
+                "content-type": "application/json",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin"
+                }
+        
+            d1 = """{
+                "reason": "311",
+                "object_id": "%s",
+                "owner_id": "%s",
+                "report_type": "user"
+                }""" % (uid, uid)
+        
+            r1 = requests.post("https://www.tiktok.com/node/report/reasons_put?", headers=h1, data=d1, verify=False, timeout=999999)
+        
+            if r1.ok:
+                r1json = r1.json()
+                #if r1json.get("statusCode") == True:
+                      #  try:
+                          #  print(f"[+] Report sent successfully")
+                    #    except:
+                      #       print(f"[-] Report could not be sent")
+                         #    return
+                        
+            try:
+                 print(col, v, "[+] Report send successfull") 
+            except:
+                  print(col, v, "[-] Reporr could not be sent")   
+        
+        v = 0
+        print("User ID: ", uid)
+        print("Username: ", usr)    
+                
+        while True:
+            v = v+1
+            report()
+            time.sleep(2.0 - ((time.time() - srttm) % 2.0))
+       
+       
      
                 
                   
@@ -409,7 +508,7 @@ elif choice == 12:
                 socks.settimeout(0.03)
                 socks.connect_ex(addr)
                 #socks.send(data)
-                print((a), '>>conectado>>', ip, 'en el purto', (port))
+                print("|",(a), "| [*] successfully | ", (port))
                 a = a+1
             except socket.error:
                 print('[#]Conection Error')
@@ -461,4 +560,10 @@ elif choice == 14:
                         siInternet = False
                 return  siInternet
     inter = InternetOk()
-    print (inter.Internet())                                                                                
+    print (inter.Internet())                                                           
+    
+elif choice == 15:
+    print("Bash > python3.9 > Linux > 2022")
+    while True:
+        a = input(">>>")
+        os.system(a)    
